b07d29f1e8bdb41db13dbab9104c8b40c1086b9a
avi chhitwal <122198966+avichhitwal@users.noreply.github.com>
2026-02-03 10:23:22 +0530

    Delete first directory

--- first/src/main/java/com/example/repository/FlightRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.example.model.Flight;
-
-public interface FlightRepository extends CrudRepository<Flight,Long>{
-
-}
